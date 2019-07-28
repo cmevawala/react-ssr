cae992523cf12ad51aa669eb09957ed825b4cb83
cmevawala <chintu.m18@gmail.com>
2019-07-28 13:32:35 +0530

    33. Browser Store Creation

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes.jsx';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk))
+
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
 , document.querySelector('#root'));
